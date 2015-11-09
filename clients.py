62a2e8734dbd808069d829651c806b9dd3c3352a
gideonsiburian <dionadiprana@gmail.com>
2015-11-09 23:56:02 +0700

    COMMIT

--- clients.py
+++ clients.py
@@ -1,79 +1,24 @@
-import socket, select, string, sys
+import sys
+import socket
 
-
-def prompt() :
-	sys.stdout.write('<You> : ')
-	sys.stdout.flush()
-
-def registrate() :
-	sys.stdout.write('Masukkan username anda : ')
-	username = sys.stdin.readline().rstrip('\n')
-	
-	if username :
-		s.send(username) 
-		respons = s.recv(LIMIT)
-		if str(respons) == 'false' and respons != None and respons != None :
-			print ' Username tersebut sudah terdaftar !'
-		if str(respons) == 'true' and respons != None  and respons != False :
-			print ' Username anda adalah '+ username
-			return True
-	else :
-		print 'Username harus terisi !'
-	
-#main function
-if __name__ == "__main__" :
-	
-	if(len(sys.argv) < 3):
-		print ' Cara menggunakan : python clients.py <hostname> <port>'
-		sys.exit()
-		
-		
-	host = sys.argv[1]
-	port = int (sys.argv[2])
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	s.settimeout(2)
-	
-	try :
-		s.connect((host, port))
-	except :
-		print 'Gagal konek ke server'
-		sys.exit()
-		
-	LIMIT = 4096
-	
-	
-	print 'Koneksi ke server sukses!\nSilahkan Login'
-	Flag = False
-	Flag = registrate()
-	
-	while Flag!=True:
-		Flag=registrate()
-	
-	prompt()
-	
-	while True:
-		list_socket = [sys.stdin, s]
-		read_sockets, write_sockets, error_sockets = select.select(list_socket, [], [])
-		
-		for sock in read_sockets :
-			if sock == s : #ada pesan masuk dari server
-				data = s.recv(LIMIT)
-				if data != None and data != False:
-					#print data
-					sys.stdout.write(data)
-					prompt()
-				
-			else : #user memasukkan pesan
-				msg = sys.stdin.readline().rstrip('\n')
-				
-				if str(msg) =='keluar' :
-					s.send(msg)
-					sys.stdout.write('Memutuskan sambungan ke server...\n')
-					sys.exit()
-				else :
-					s.send(msg) 
-					prompt() #user mengirim pesan
-					
-	sys.exit()
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
+try:
+	# Send data
+	message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
+	print >>sys.stderr, 'sending "%s"' % message
+	sock.sendall(message)
+	# Look for the response
+	amount_received = 0
+	amount_expected = len(message)
+	while amount_received < amount_expected:
+		data = sock.recv(16)
+		amount_received += len(data)
+		print >>sys.stderr, 'received "%s"' % data
+finally:
+	print >>sys.stderr, 'closing socket'
+	sock.close()
--- server.py
+++ server.py
@@ -1,111 +1,56 @@
+import threading
 import socket
-import string
-import select
+import time
 import sys
 
-def namebysocket(socket):
-	indeks = Sockets.index(socket)
-	senderName = Clients[indeks]
-	return str(senderName)
+def get_file(nama):
+	myfile = open(nama)
+	return myfile.read()
 
-def socketbyname(name):
-	indeks = Clients.index(name)
-	socket = Sockets[indeks]
-	return socket
+class MemprosesClient(threading.Thread):
+	def __init__(self,client_socket,client_address,nama):
+		self.client_socket = client_socket
+		self.client_address = client_address
+		self.nama = nama
+		threading.Thread.__init__(self)
 
-def broadcast (sock, msg):	#sock : socket si pengirim data
-	senderName = namebysocket(sock)
-	message = '\r' + senderName + ': ' + msg + '\n'
-	for socket in Sockets :
-		if socket != sock_server and socket!= sock :
-			socket.send(message)
+	def run(self):
+		message = ''
+		while True:
+			data = self.client_socket.recv(32)
+			if data:
+				message = message + data 
+				#print message
+				#collect seluruh data yang diterima
+				if (message.endswith("\r\n\r\n")):
+					#print 'Client finish'
+					self.client_socket.send('HTTP/1.1 200 OK\r\n\r\n'+get_file('gambar.jpg'))
+					#self.client_socket.send('HTTP/1.1 200 OK\r\n\r\ntes')
+					break
+			else:
+				break
+		self.client_socket.close()
 
-def sendto(sockFrom, nameTo, msg):
-	senderName = namebysocket(sockFrom)
-	if nameTo in Clients :
-		message = '<' + senderName + '> : ' + msg + '\n'
-		sockTo = socketbyname(nameTo)
-		sockTo.send(message)
-	else :
-		sockFrom.send('\r Username yang dituju belum terdaftar\n')
+class Server(threading.Thread):
+	def __init__(self):
+		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.server_address = ('127.0.0.1',9999)
+		self.my_socket.bind(self.server_address)
+		threading.Thread.__init__(self)
+		print 'Server is running!'
 
-def storeNewClientData ( newClient_socket, newClient_name ):
-	Clients.append(newClient_name)
-	Sockets.append(newClient_socket)
-	#notifikiasi di server
-	print newClient_name +' sekarang terhubung '
-	
-	broadcast (sockfd,newClient_name + ' sekarang terhubung ke server.')
-	
-	
-def client_isOffline ( off_socket ):
-	off_name = namebysocket(off_socket)
-	Sockets.remove(off_socket)
-	print off_name +' terputus dari server.'
-	broadcast (sockfd, off_name + ' terputus dari server.')
-	Clients.remove(off_name)
-	
-
-def listClients( sockRequested ):
-	print namebysocket(sockRequested) +' list user online.'
-	sockRequested.send('\r    List User Online')
-	for client in Clients :
-			if client == 'Server':
-				print 'ok'
-			else :
-				sockRequested.send('\r    Online : ' + client + '\n')
-
-		
-
-if __name__ == "__main__" :
-	
-	Clients = [] #array of clients name
-	Sockets = [] #array of sockets
-	
-	LIMIT = 4096
-	HOST = "0.0.0.0"
-	PORT = 5000
-	sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reuse address
-	sock_server.bind((HOST, PORT))
-	sock_server.listen(10)
-	
-	Clients.append('Server')
-	Sockets.append(sock_server)
-	print "\nServer terhubung ke jaringan ! Port server : " + str(PORT)
-	
-	while True:
-		read_sockets, write_sockets, error_sockets = select.select(Sockets,[],[])
+	def run(self):
+		self.my_socket.listen(1)
+		nomor=0
+		while (True):
+			self.client_socket, self.client_address = self.my_socket.accept()
+			#print 'Another client is connecting'
+			nomor=nomor+1
+			#---- menghandle message cari client (Memproses client)
+			my_client = MemprosesClient(self.client_socket, self.client_address, 'PROSES NOMOR '+str(nomor))
+			my_client.start()
 			
-		for sock in read_sockets:
-			if sock == sock_server : 
-				sockfd, addr = sock_server.accept()
-				
-				tanda = False
-				while tanda!=True :
-					uname = sockfd.recv(LIMIT)
-					if uname!=None and uname!=False :
-						if uname in Clients :
-							sockfd.send('false')
-						else :
-							sockfd.send('true')
-							storeNewClientData(sockfd, uname)
-							tanda=True
-							
-			else :
-				msg = sock.recv(LIMIT)
-				if msg :
-					if msg =='keluar' :
-						client_isOffline(sock) #kirim data socket client yang exiting
-					else :
-						arrayMsg = msg.split(' ',2)
-						if arrayMsg[0]=='sendto' :
-							nameTo = arrayMsg[1]
-							sendto(sock, nameTo, arrayMsg[2] )
-						elif arrayMsg[0]=='list' :
-							listClients(sock)
-							print '\n'
-						else :
-							broadcast (sock, msg)
-	sock_server.close()
-	sys.exit()
+#----
+serverku = Server()
+serverku.start()
